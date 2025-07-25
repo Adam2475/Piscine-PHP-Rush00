c0954880f5685b41347d021f4341e5bf8a07aef9
Adam2475 <adamnatanpassaretti@gmail.com>
2025-07-25 19:47:30 +0200

    player movement working

--- moviemon/src/Controller/MenuController.php
+++ moviemon/src/Controller/MenuController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+// including the Game Manager service
+use App\Service\GameManager;
+
 class MenuController extends AbstractController
 {
 
--- moviemon/src/Controller/OverworldController.php
+++ moviemon/src/Controller/OverworldController.php
@@ -6,32 +6,61 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+// including the Game Manager service
+use App\Service\GameManager;
+
 class OverworldController extends AbstractController
 {
+    //TODO: aggiungere grandezza della mappa al game manager
+    #[Route('/new', name: 'new')]
+    public function StartNewGame(GameManager $gameManager)
+    {
+        $gameManager->StartNewGame("testUser");
+        $user = $gameManager->getUser();
+        $position = array(2, 3);
+        $user->setPosition($position);
+        return $this->redirectToRoute('overworld');
+    }
 
     #[Route('/overworld', name: 'overworld')]
-    public function index(): Response
+    public function index(GameManager $gameManager): Response
     {
-        $map = $this->generateMap();
+        $map = $this->generateMap($gameManager);
         return $this->render('map.html.twig', [
             'map' => $map,
         ]);
     }
 
-    public function generateMap()
+    public function generateMap(GameManager $gameManager)
     {
         $grid = [];
 
         $rows = 5;
         $columns = 5;
 
+        $user = $gameManager->getUser();
+
+        // var_dump($user);
+
+        // finding center of the map
+        // $playerX = (int) floor($rows / 2);
+        // $playerY = (int) floor($columns / 2);
+
+        // $playerPosition = [$playerX, $playerY];
+        $playerPosition = $user->getPosition();
+
+        // debug
+        // echo "player X: " . $playerPosition[0] . "  player Y: " . $playerPosition[1];
+
         for ($y = 0; $y < $columns; $y++) {
             $row = [];
             for ($x = 0; $x < $rows; $x++) {
-                $row[] = "[$x,$y]";
+                $row[] = " ";
             }
             $grid[] = $row;
         }
+
+        $grid[$playerPosition[0]][$playerPosition[1]] = 'P';
 
         return ($grid);
     }
