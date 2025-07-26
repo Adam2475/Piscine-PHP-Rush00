21caa271567e957c88bd786148b85ff8f2902b69
marcotani <marco.tani92@gmail.com>
2025-07-26 17:51:54 +0200

    Cambiato la encounter rate a 20% (per davvero stavolta, sono un idiota)

--- moviemon/src/Service/GameManager.php
+++ moviemon/src/Service/GameManager.php
@@ -117,7 +117,7 @@ class GameManager
             return null;
         $i = random_int(0, $n_moviemons - 1);
 
-        if (random_int(1, 100) <= 20)
+        if (random_int(1, 100) > 20)
             return null;
         return $moviemons[$i] ?? null;
     }
